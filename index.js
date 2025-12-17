5db3d779e8a7fe83bb47e88ffecfc6046f540805
Anna-Gladush <ann.gladush@gmail.com>
2025-12-17 15:03:44 +0200

    Size is determined by user input. User Input is being checked if it's a valid input (number)

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 const container = document.querySelector('.container');
-let size = 16;
-const margin = 5;
-const minCellSize = 40;                 // во сколько пикселей минимум одна клетка
-const cellSize = minCellSize;           // можно потом масштабировать
-const boardSize = size * (cellSize + margin * 2);
+const margin = 2;
+const border = 1;
 
+//USER INPUT
+let userInput = prompt("What sketchpad size do you want? (enter only numbers)");
+let size = parseInt(userInput);
+
+if (userInput === 'null' || typeof userInput !== 'number') {
+    size = prompt('Your input is wrong. Please type a number');
+}
+
+
+// chose height to scale (because it's smaller then width)
+const boardSize = window.innerHeight * 0.9; 
 
 container.style.width = boardSize + 'px';
 
-for (let i = 0; i < size * size; i++) {
-    const square  = document.createElement('div');
-    square.classList.add('row');
-    square.style.width = cellSize + 'px';
-    square.style.height = cellSize + 'px';
-    container.appendChild(square);
-}
+
+    const cellSize = (boardSize / size) - 2 * margin;
+
+    for (let i = 0; i < size * size; i++) {
+        const square  = document.createElement('div');
+        square.classList.add('row');
+        square.style.width = cellSize + 'px';
+        square.style.height = cellSize + 'px';
+        container.appendChild(square);
+        console.log(size)
+    }
+
