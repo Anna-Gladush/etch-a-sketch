b9e5e13089ab985ea5669fcde5d87702d71f38a9
Anna-Gladush <ann.gladush@gmail.com>
2025-12-17 15:59:09 +0200

    User input is accessed through HTML input. To make another grid, refresh

--- index.js
+++ index.js
@@ -2,20 +2,13 @@ const container = document.querySelector('.container');
 const margin = 2;
 const border = 1;
 
-//USER INPUT
-let userInput = prompt("What sketchpad size do you want? (enter only numbers)");
-let size = parseInt(userInput);
+drawSketchPad();
 
-if (userInput === 'null' || typeof userInput !== 'number') {
-    size = prompt('Your input is wrong. Please type a number');
-}
-
-
+function drawGrid(size) {
 // chose height to scale (because it's smaller then width)
-const boardSize = window.innerHeight * 0.9; 
+    const boardSize = window.innerHeight * 0.9; 
 
-container.style.width = boardSize + 'px';
-
+    container.style.width = boardSize + 'px';
 
     const cellSize = (boardSize / size) - 2 * margin;
 
@@ -27,4 +20,18 @@ container.style.width = boardSize + 'px';
         container.appendChild(square);
         console.log(size)
     }
+}
 
+function drawSketchPad() {
+    let size;
+    const text = document.querySelectorAll('.text');
+    const input = document.querySelector('#number');
+    input.addEventListener("keydown", (e) => {
+        if (e.key === 'Enter') {
+            size = e.target.value;
+            input.style.display = 'none';
+            text.forEach(p => {p.textContent = '';})
+            console.log('Enter нажат, size:', size);
+        }
+        drawGrid(size);
+})}
