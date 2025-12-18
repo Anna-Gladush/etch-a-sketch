ec517c834ff0245342ad2ee8b48bfded2e141262
Anna-Gladush <ann.gladush@gmail.com>
2025-12-18 15:00:35 +0200

    ver. 2.0.0. rewrote a few functions, added buttons, changed how user interacts with sketchpad

--- README.md
+++ README.md
@@ -2,4 +2,9 @@ Version 1.5.0.
 - You can change grid
 - You can draw (random color)
 
-![example](https://github.com/Anna-Gladush/etch-a-sketch/blob/main/images/image.png)
+![example-1](https://github.com/Anna-Gladush/etch-a-sketch/blob/main/images/image.png)
+
+Version 2.0.0
+- You can choose a color to draw
+
+![example-2](https://github.com/Anna-Gladush/etch-a-sketch/blob/main/images/image-1.png)
--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const container = document.querySelector('.container');
-const gap = 2;
-const border = 1;
-
+const h1 = document.querySelector('h1');
 drawSketchPad();
 
 function drawGrid(size) {
@@ -11,33 +9,96 @@ function drawGrid(size) {
         square.style.height = 540 / size + 'px';
         square.classList.add('row');
         container.appendChild(square);
+        container.style.border = 'solid 1px rgb(195, 195, 195)';
     }
-    console.log('total cells:', container.children.length); 
 }
 
 function drawSketchPad() {
     let size;
     const text = document.querySelectorAll('.text');
     const input = document.querySelector('#number');
-    const btn = document.querySelector('.hiddenRefreshButton')
-    input.addEventListener("keydown", (e) => {
+    const btn = document.querySelector('.hiddenRefreshButton');
+    const color = document.querySelectorAll('.col');
+    input.addEventListener('keydown', (e) => {
         if (e.key === 'Enter') {
             size = e.target.value;
             if (size >= 100) {
                 document.querySelector('#prompt').textContent = 'The number is more than 100. Please enter a lower number';
                 document.querySelector('#pressEnter').textContent = '';
                 return;
+            } else if (size <= 0) {
+                document.querySelector('#prompt').textContent = 'The number is negative or equals 0. Please enter a higher number';
+                document.querySelector('#pressEnter').textContent = '';
+                return;
             }
             if (size < 100) {
+                color.forEach((color) => color.style.display = 'block')
                 input.style.display = 'none';
                 text.forEach(p => {p.textContent = '';});
                 btn.style.display='block';
+                h1.innerText = '';
                 drawGrid(size);
             }
         }
         coloring();
         refresh();
 })}
+
+function refresh(){
+    const color = document.querySelectorAll('.col');
+    const btn = document.querySelector('.hiddenRefreshButton');
+    btn.addEventListener('click', () => {
+        const prompt = document.querySelector('#prompt');
+        const pressEnter = document.querySelector('#pressEnter');
+        const input = document.querySelector('#number');
+        const div = document.querySelectorAll('.row')
+        input.value = '';
+        input.style.display = 'block';
+        prompt.textContent = 'What sketchpad size do you want? (enter only positive numbers)';
+        pressEnter.textContent = 'Press Enter';
+        container.style.border = 'none';
+        h1.innerText = 'Yours truly presents to you: Etch-a-Sketch';
+        color.forEach((color) => color.style.display = 'none');
+        while (container.firstChild) {
+            container.removeChild(container.firstChild);
+        }
+
+        btn.style.display='none';
+    })
+}
+
+function coloring() {
+    let square = document.querySelectorAll('.row');
+    let btn = document.querySelectorAll('.col')
+
+    btn.forEach((color) => { color.addEventListener('click', () => {
+        if (color.classList.contains('red')) {
+            square.forEach((cell) => {
+            cell.addEventListener('mouseenter', () => cell.style.backgroundColor = `#EF476F`)})
+        } else if (color.classList.contains('orange')) {
+            square.forEach((cell) => {
+            cell.addEventListener('mouseenter', () => cell.style.backgroundColor = `#F78C6B`)})
+        } else if (color.classList.contains('yellow')) {
+            square.forEach((cell) => {
+            cell.addEventListener('mouseenter', () => cell.style.backgroundColor = `#FFD166`)})
+        } else if (color.classList.contains('green')) {
+            square.forEach((cell) => {
+            cell.addEventListener('mouseenter', () => cell.style.backgroundColor = `#06D6A0`)})
+        } else if (color.classList.contains('blue')) {
+            square.forEach((cell) => {
+            cell.addEventListener('mouseenter', () => cell.style.backgroundColor = `#118AB2`)})
+        }  else if (color.classList.contains('darkblue')) {
+            square.forEach((cell) => {
+            cell.addEventListener('mouseenter', () => cell.style.backgroundColor = `#073B4C`)})
+        } else if (color.classList.contains('random')) {
+            randomColoring();
+        }else if (color.classList.contains('clear')) {
+            square.forEach((cell) => cell.style.backgroundColor = "white")
+        }
+        })
+    })
+
+}
 
 function randomColoring() {
 let square = document.querySelectorAll('.row');
@@ -49,67 +110,4 @@ square.forEach((cell) => {
         cell.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
     })
 })
-
 }
-
-function refresh(){
-    const btn = document.querySelector('.hiddenRefreshButton');
-    btn.addEventListener('click', () => {
-        const prompt = document.querySelector('#prompt');
-        const pressEnter = document.querySelector('#pressEnter');
-        const input = document.querySelector('#number');
-        const div = document.querySelectorAll('.row')
-        input.value = '';
-        input.style.display = 'block';
-        prompt.textContent = 'What sketchpad size do you want? (enter only positive numbers)';
-        pressEnter.textContent = 'Press Enter';
-        
-        while (container.firstChild) {
-            container.removeChild(container.firstChild);
-        }
-
-
-        btn.style.display='none';
-    })
-    //btn.classList.remove('.toggle')
-}
-
-function coloring() {
-    let square = document.querySelectorAll('.row');
-    let btn = document.querySelectorAll('.col')
-    let red = document.querySelector('.red')
-    let blue = document.querySelector('.blue')
-    let green = document.querySelector('.green')
-    let yellow = document.querySelector('.yellow')
-    let violet = document.querySelector('.violet')
-    let clear = document.querySelector('.clear')
-
-    // btn.forEach((color) => addEventListener("click", () => ))
-    btn.forEach((color) => { color.addEventListener('click', () => {
-        if (color.classList.contains('red')) {
-            square.forEach((cell) => {
-            cell.addEventListener('mouseenter', () => cell.style.backgroundColor = `red`)})
-        } else if (color.classList.contains('green')) {
-            square.forEach((cell) => {
-            cell.addEventListener('mouseenter', () => cell.style.backgroundColor = `green`)})
-        } else if (color.classList.contains('yellow')) {
-            square.forEach((cell) => {
-            cell.addEventListener('mouseenter', () => cell.style.backgroundColor = `yellow`)})
-        } else if (color.classList.contains('blue')) {
-            square.forEach((cell) => {
-            cell.addEventListener('mouseenter', () => cell.style.backgroundColor = `blue`)})
-        } else if (color.classList.contains('violet')) {
-            square.forEach((cell) => {
-            cell.addEventListener('mouseenter', () => cell.style.backgroundColor = `purple`)})
-        }
-    })
-})
-            
-    //     red.addEventListener('click', () => {
-    //         red.classList.add('active');
-    //         if (red.classList.contains('active')) {
-    //         square.forEach((cell) => {
-    //         cell.addEventListener('mouseenter', () => cell.style.backgroundColor = `red`)})}}
-    //     )
-    // }
-    }
