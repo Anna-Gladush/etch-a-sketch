4c93c340defd3f363623a9f8e98a2439c3980614
Anna-Gladush <ann.gladush@gmail.com>
2025-12-18 13:28:13 +0200

    Added color choosing

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ drawSketchPad();
 function drawGrid(size) {
     for (let i = 0; i < size * size; i++) {
         const square  = document.createElement('div');
-         square.style.width = 600 / size + 'px';
-        square.style.height = 600 / size + 'px';
+         square.style.width = 540 / size + 'px';
+        square.style.height = 540 / size + 'px';
         square.classList.add('row');
         container.appendChild(square);
     }
@@ -35,7 +35,7 @@ function drawSketchPad() {
                 drawGrid(size);
             }
         }
-        randomColoring();
+        coloring();
         refresh();
 })}
 
@@ -47,9 +47,9 @@ square.forEach((cell) => {
         const g = Math.floor(Math.random() * 256);
         const b = Math.floor(Math.random() * 256);
         cell.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-        cell.addEventListener("click", () => cell.style.backgroundColor = `white`)
     })
 })
+
 }
 
 function refresh(){
@@ -73,3 +73,43 @@ function refresh(){
     })
     //btn.classList.remove('.toggle')
 }
+
+function coloring() {
+    let square = document.querySelectorAll('.row');
+    let btn = document.querySelectorAll('.col')
+    let red = document.querySelector('.red')
+    let blue = document.querySelector('.blue')
+    let green = document.querySelector('.green')
+    let yellow = document.querySelector('.yellow')
+    let violet = document.querySelector('.violet')
+    let clear = document.querySelector('.clear')
+
+    // btn.forEach((color) => addEventListener("click", () => ))
+    btn.forEach((color) => { color.addEventListener('click', () => {
+        if (color.classList.contains('red')) {
+            square.forEach((cell) => {
+            cell.addEventListener('mouseenter', () => cell.style.backgroundColor = `red`)})
+        } else if (color.classList.contains('green')) {
+            square.forEach((cell) => {
+            cell.addEventListener('mouseenter', () => cell.style.backgroundColor = `green`)})
+        } else if (color.classList.contains('yellow')) {
+            square.forEach((cell) => {
+            cell.addEventListener('mouseenter', () => cell.style.backgroundColor = `yellow`)})
+        } else if (color.classList.contains('blue')) {
+            square.forEach((cell) => {
+            cell.addEventListener('mouseenter', () => cell.style.backgroundColor = `blue`)})
+        } else if (color.classList.contains('violet')) {
+            square.forEach((cell) => {
+            cell.addEventListener('mouseenter', () => cell.style.backgroundColor = `purple`)})
+        }
+    })
+})
+            
+    //     red.addEventListener('click', () => {
+    //         red.classList.add('active');
+    //         if (red.classList.contains('active')) {
+    //         square.forEach((cell) => {
+    //         cell.addEventListener('mouseenter', () => cell.style.backgroundColor = `red`)})}}
+    //     )
+    // }
+    }
